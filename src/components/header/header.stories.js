a88d7cf962f767a09fb6eeab52f44974315fe083
oersoy91 <osman_ersoy@live.de>
2021-01-29 11:27:10 +0100

    header

---
+++ src/components/header/header.stories.js
@@ -0,0 +1,9 @@
+import "./header.css";
+import header from "./header.html";
+
+export default {
+  title: "Components/Header",
+  parameters: { layout: "centered" },
+};
+
+export const itemheader = () => header;
