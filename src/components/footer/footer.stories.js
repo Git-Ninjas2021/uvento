da29d82533a0f073734bc57142a1ff1922a83409
RafaLindemann <lindemannrafael@icloud.com>
2021-01-29 14:46:39 +0100

    add footer

---
+++ src/components/footer/footer.stories.js
@@ -0,0 +1,9 @@
+import "./footer.css";
+import footer from "./footer.html";
+
+export default {
+  title: "Components/Footer",
+  parameters: { layout: "centered" },
+};
+
+export const itemfooter = () => footer;
